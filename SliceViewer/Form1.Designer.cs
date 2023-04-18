e47f7a18becc271ec55ebbfeb97737f29b502a66
Chinyi Cheng <yoda670612@gmail.com>
2023-04-18 11:30:33 +0900

    Add project files.

---
+++ SliceViewer/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace SliceViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(428, 465);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBar1.Location = new System.Drawing.Point(68, 486);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(283, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 552);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ SliceViewer/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BitMiracle.LibTiff;
+using BitMiracle.LibTiff.Classic;
+
+namespace SliceViewer
+{
+    public partial class Form1 : Form
+    {
+        private Tiff tiff;
+        private int numberOfPages;
+        public Form1()
+        {
+            InitializeComponent();
+            tiff = Tiff.Open("Haase_MRT_tfl3d1.tif", "r");
+            numberOfPages = tiff.NumberOfDirectories();
+            tiff.SetDirectory(0);
+            Bitmap bmp = GetBitmapFormTiff(tiff);
+            pictureBox1.Image = bmp;
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = numberOfPages;
+        }
+
+
+        private static Bitmap GetBitmapFormTiff(Tiff tif)
+        {
+            FieldValue[] value = tif.GetField(TiffTag.IMAGEWIDTH);
+            int width = value[0].ToInt();
+
+            value = tif.GetField(TiffTag.IMAGELENGTH);
+            int height = value[0].ToInt();
+
+            //Read the image into the memory buffer
+            var raster = new int[height * width];
+            if (!tif.ReadRGBAImage(width, height, raster))
+            {
+                return null;
+            }
+
+            var bmp = new Bitmap(width, height, PixelFormat.Format32bppRgb);
+
+            var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+
+            BitmapData bmpdata = bmp.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format32bppRgb);
+            var bits = new byte[bmpdata.Stride * bmpdata.Height];
+
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                int rasterOffset = y * bmp.Width;
+                int bitsOffset = (bmp.Height - y - 1) * bmpdata.Stride;
+
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    int rgba = raster[rasterOffset++];
+                    bits[bitsOffset++] = (byte)((rgba >> 16) & 0xff);
+                    bits[bitsOffset++] = (byte)((rgba >> 8) & 0xff);
+                    bits[bitsOffset++] = (byte)(rgba & 0xff);
+                    bits[bitsOffset++] = (byte)((rgba >> 24) & 0xff);
+                }
+            }
+
+            System.Runtime.InteropServices.Marshal.Copy(bits, 0, bmpdata.Scan0, bits.Length);
+            bmp.UnlockBits(bmpdata);
+
+            return bmp;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Console.WriteLine(trackBar1.Value);
+            int currentPage = trackBar1.Value;
+            tiff.SetDirectory((short)currentPage);
+            Bitmap bmp = GetBitmapFormTiff(tiff);
+            pictureBox1.Image = bmp;
+        }
+    }
+}
